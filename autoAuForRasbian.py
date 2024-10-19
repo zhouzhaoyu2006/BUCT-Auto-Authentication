110cdf68e4d1030f51d8c9fc5e9750e033f3bb7e
Pyhtagodzilla <1670671958@qq.com>
2024-10-19 16:18:38 +0800

    Raspbian

---
+++ autoAuForRasbian.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome()
+
+browser.get("https://tree.buct.edu.cn/index_20.html")
+
+try:
+    browser.implicitly_wait(10)
+    browser.find_element(By.XPATH,'//*[@id="username"]').send_keys('2024050276')
+    browser.find_element(By.XPATH,'//*[@id="password"]').send_keys('@Jtbx2mtblj')
+    browser.find_element(By.XPATH,'//*[@id="login-account"]').click()
+except Exception:
+    print('Authentication finish!')
